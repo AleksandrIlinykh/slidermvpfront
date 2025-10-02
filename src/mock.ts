d3821c39ae6f1f3e40dfe2c1de81c254f94e7eb4
AleksandrIlinykh <AleksandrIlinykhDev@gmail.com>
2025-10-02 18:39:32 +0200

    Fix filenames

--- src/mock.ts
+++ src/mock.ts
@@ -4,15 +4,15 @@ export const defaultConfig: SimpleConfig = {
   images: [
     {
       title: '2013 Thor Motor Coach TUSCANY XTE 40EX',
-      url: './2013 Thor Motor Coach TUSCANY XTE 40EX.jpg',
+      url: './2013_Thor_Motor_Coach_TUSCANY_XTE_40EX.jpg',
     },
     {
       title: '2016 Jayco North Point 377RLBH',
-      url: './2016 Jayco North Point 377RLBH.jpg',
+      url: './2016_Jayco_North_Point_377RLBH.jpg',
     },
     {
       title: '2022 Prime Time AVENGER 25 FLS',
-      url: './2022 Prime Time AVENGER 25 FLS.jpg',
+      url: './2022_Prime_Time_AVENGER_25_FLS.jpg',
     },
   ],
 };
